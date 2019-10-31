0bda6abc19f8e441c22cad2be2824ab39da7c912
ivmtorres <ivmtorres@yahoo.com.ar>
2019-10-30 23:19:18 -0300

    re test

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const app = express()
-const getCachedSensorReadings = require('./get-cached-sensor-readings')
+const getCachedSensorReadings = require('./get-cached-sensor-readings.1')
 
 /*
 Here, we are introduced to express middleware.
@@ -11,7 +11,7 @@ that have to take place before the request handler.
 In the below statement, we use the express.static 
 middleware, and bind it to the /public route.
 */
-app.use('/public', express.static(path.join(__dirname,'public')))
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 app.get('/temperature', function (req, res) {
   res.send('<strong>' + 
@@ -20,7 +20,8 @@ getCachedSensorReadings.getTemperature().toFixed(1) +
 })
 
 app.get('/humidity', function (req, res) {
-  res.send('<strong>'+getCachedSensorReadings.getHumidity().toFixed(1) + '</strong>'+'%')
+  res.send(getCachedSensorReadings.getHumidity
+().toFixed(1) + '%')
 })
 
 app.listen(3000, function () {
